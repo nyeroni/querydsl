ed1fd9702cf8ed6b91fe7894d89a5afb40e181f4
nyeroni <nir2y@naver.com>
2024-01-26 16:40:50 +0900

    Update: 필드 직접 접근(fields 방식)

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -485,6 +485,15 @@ public class QuerydslBasicTest {
             System.out.println("memberDto = "+ memberDto);
         }
     }
-
+    @Test
+    public void findDtoByField(){
+        List<MemberDto> result = queryFactory
+                .select(Projections.fields(MemberDto.class, member.username, member.age))
+                .from(member)
+                .fetch();
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = "+ memberDto);
+        }
+    }
 
 }
