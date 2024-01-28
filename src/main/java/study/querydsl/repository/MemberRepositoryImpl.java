36dea16bbfa965701fcd499cd7c9cf2f9d09a995
nyeroni <nir2y@naver.com>
2024-01-29 01:30:27 +0900

    Update: 전체 카운트를 한번에 조회하는 방법, searchPageSimple(), fetchResults() 사용

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,8 +1,12 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -41,6 +45,34 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                         ageLoe(condition.getAgeLoe()))
                 .fetch();
     }
+    /**
+     * 단순한 페이징, fetchResults() 사용
+     */
+    @Override
+    public Page<MemberTeamDto> searchPageSimple (MemberSearchCondition condition, Pageable pageable){
+        QueryResults<MemberTeamDto> results = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.username,
+                        member.age,
+                        team.id,
+                        team.name
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+        List<MemberTeamDto> content = results.getResults();
+        long total = results.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
+    }
 
     private BooleanExpression usernameEq(String username){
         return isEmpty(username) ? null : member.username.eq(username);
@@ -54,4 +86,6 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
     private BooleanExpression ageLoe(Integer ageLoe) {
         return ageLoe == null ? null : member.age.loe(ageLoe);
     }
+
+
 }
